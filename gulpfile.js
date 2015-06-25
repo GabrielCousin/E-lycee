673f5f4dd15d8eb5151a84aaed0c641aeccad711
Gabriel Cousin <csngab@gmail.com>
2015-06-25 19:17:09 +0200

    Gulp | màj des tâches

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,9 @@ var minifyCSS     = require('gulp-minify-css');
 
 // Build
 gulp.task('build', function(){
-  return gulp.src('web/front/scss/app.scss')
+  return gulp.src('web/dev/scss/app.scss')
     .pipe(plumber())
-    .pipe(sass())
+    .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions']
     }))
@@ -21,5 +21,20 @@ gulp.task('build', function(){
     .pipe(gulp.dest('web/css'));
 });
 
+// Dev
+gulp.task('css:dev', function(){
+  return gulp.src('web/dev/scss/app.scss')
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('web/css'));
+});
+
 // Tasks
 gulp.task('default', ['build']);
+gulp.task('dev', ['css:dev'], function(){
+  gulp.watch(["web/dev/scss/**/*.scss"], ['css:dev'])
+});
