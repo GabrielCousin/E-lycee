8d4f1937f07def1cb0c8c32bf1803f7f0ce69ee8
Gabriel Cousin <csngab@gmail.com>
2015-06-30 10:44:49 +0200

    Gulp | gestion des erreurs en LiveReload

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,12 @@ var sass          = require('gulp-sass');
 var autoprefixer  = require('gulp-autoprefixer');
 var minifyCSS     = require('gulp-minify-css');
 var concat        = require('gulp-concat');
+var gutil         = require('gulp-util');
 
 
 gulp.task('bs', ['css:dev'], function() {
   browserSync.init({
-      host: "localhost:8000",
+      host: "172.0.0.1:8000",
       ghostMode: false,
       ui: false
   });
@@ -36,13 +37,21 @@ gulp.task('css:build', function(){
     .pipe(gulp.dest('web/css'));
 });
 
+gulp.task('js', function() {
+  return gulp.src(['web/dev/vendors/jquery/dist/jquery.js', 'web/dev/vendors/materialize/dist/js/materialize.js'])
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('web/js'));
+});
 
 
 // Dev
-gulp.task('css:dev', function(){
+gulp.task('css:dev', function() {
   return gulp.src('web/dev/scss/app.scss')
-    .pipe(plumber())
-    .pipe(sass(sass.sync().on('error', sass.logError)))
+		.pipe(plumber(function(error) {
+				gutil.log(gutil.colors.red(error.message));
+				this.emit('end');
+		}))
+		.pipe(sass())
     .pipe(autoprefixer({
       browsers: ['last 2 versions']
     }))
@@ -51,14 +60,25 @@ gulp.task('css:dev', function(){
     .pipe(browserSync.stream());
 });
 
-gulp.task('js', function() {
-  return gulp.src(['web/dev/vendors/jquery/dist/jquery.js', 'web/dev/vendors/materialize/dist/js/materialize.js'])
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('web/js'));
+gulp.task('css:watch', function() {
+	return gulp.watch(['web/dev/scss/**/*.scss'], ['css:dev', 'bs:reload']);
 });
+
+gulp.task('bs:serve', function () {
+	browserSync.init({
+      host: "172.0.0.1:8000",
+      ghostMode: false,
+      ui: false
+  });
+});
+
+gulp.task('bs:reload', function () {
+	return browserSync.reload({
+		stream: true
+	});
+});
+
 
 // Tasks
 gulp.task('default', ['css:build', 'js']);
-gulp.task('dev', ['css:dev', 'bs'], function(){
-  gulp.watch(["web/dev/scss/**/*.scss"], ['css:dev'])
-});
+gulp.task('dev', ['css:dev', 'css:watch', 'bs:serve']);
