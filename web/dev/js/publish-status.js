8a4d17e169a3e1b71048f3cd2e06178462ca5150
Gabriel Cousin <csngab@gmail.com>
2015-07-29 14:13:53 +0200

    Suppression d'items | réécriture en JS natif

--- web/dev/js/publish-status.js
+++ web/dev/js/publish-status.js
@@ -1,31 +1,59 @@
-$('.btn-editPublish').on('click', function(e) {
-  e.preventDefault();
-  var id = $(this).data('id');
-  if ($(this).data('type')=== 'post')
-    var urlAjax = ( Routing.generate('teacher.article.editStatus',{'id':id}) );
-  else if($(this).data('type')=== 'fiche')
-    var urlAjax = ( Routing.generate('teacher.fiche.editStatus',{'id':id}) );
+var publishStatus = {
 
-  $.ajax({
-    url: urlAjax
-  }).done(function(response, status) {
-    console.log("Le post " + id + " est maintenant " + response.message);
-    $('.btn-editPublish[data-id=' + id + ']').html(response.message);
-    }).fail(function(error,status){
-      console.alert("erreur : "+error,status);
-    })
-});
+  xhr: null,
 
-$('.btn-deleteItem').on('click', function(e) {
-  e.preventDefault();
-  var id = $(this).data('id');
-  var type = $(this).data('type');
-  var valid = confirm('Êtes-vous sûr de vouloir supprimer cet item ?');
-  if (valid) {
-    // redirection et non appel ajax comme précédemment
-    if (type =="post")
-      document.location.href = Routing.generate('teacher.article.delete',{'id':id});
-    else if (type === "fiche")
-      document.location.href = Routing.generate('teacher.fiche.delete',{'id':id});
+  init: function() {
+    this.deleters = document.querySelectorAll('.btn-deleteItem');
+    this.switchs = document.querySelectorAll('.btn-editPublish');
+
+    var app = this;
+
+    for (var i = 0, l = this.deleters.length; i < l; i++) {
+      this.deleters[i].addEventListener('click', function(){ app.delete(this.getAttribute('data-id'), this.getAttribute('data-type'))}, false);
+    };
+
+    for (var i = 0, l = this.switchs.length; i < l; i++) {
+      this.switchs[i].addEventListener('click', function(){ app.switch(this.getAttribute('data-id'), this.getAttribute('data-type'))}, false);
+    };
+  },
+
+  switch: function(id, type) {
+
+    var self = this;
+
+    switch(type) {
+      case "post":
+        var route = Routing.generate('teacher.article.editStatus', {id : id}, true)
+        break;
+      case "fiche":
+        var route = Routing.generate('teacher.fiche.editStatus', {id : id}, true)
+        break;
+    }
+
+    this.xhr = new XMLHttpRequest();
+    this.xhr.open('POST', route, true);
+    this.xhr.send(null);
+    this.xhr.addEventListener('readystatechange', function(e) {
+      if(self.xhr.readyState == 4 && self.xhr.status == 200) {
+        var response = JSON.parse(self.xhr.responseText);
+        var target = document.querySelectorAll('.btn-editPublish[data-id="' + id + '"]')[0];
+        target.innerHTML = response.message;
+      }
+    }, false);
+  },
+
+  delete: function(id, type) {
+    var valid = confirm('Êtes-vous sûr de vouloir supprimer cet item ?');
+    if (valid) {
+      switch(type) {
+        case "post":
+          document.location = Routing.generate('teacher.article.delete',{'id':id})
+          break;
+        case "fiche":
+          document.location = Routing.generate('teacher.fiche.delete',{'id':id})
+          break;
+      }
+    }
   }
-});
+
+}
