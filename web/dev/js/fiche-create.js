7a348761984a54f979ebddaa4e54b2320a796a90
Gabriel Cousin <csngab@gmail.com>
2015-08-03 09:16:03 +0200

    Création de fiche | correction bug

--- web/dev/js/fiche-create.js
+++ web/dev/js/fiche-create.js
@@ -47,33 +47,32 @@ var FicheCreate = {
 
   fieldLimit: function() {
     var inputs = document.querySelectorAll('.mdl-textfield__input');
-    this.submitBtn[0].setAttribute("disabled", true);
+    this.submitBtn[0].setAttribute('disabled', true);
 
     for (var i = 0, l = inputs.length; i < l; i++) {
       inputs[i].addEventListener('blur', this.submit.bind(this), false);
     }
 
-    var addBtn = document.querySelectorAll('.fiche-question-adder');
     this.questionCount = document.querySelectorAll('.fiche-question').length;
 
     if (this.questionCount >= 5) {
-      addBtn[0].setAttribute("disabled", true);
+      this.adderBtn.setAttribute("disabled", true);
     } else {
-      addBtn[0].removeAttribute("disabled");
+      this.adderBtn.removeAttribute("disabled");
     }
   },
 
   submit: function() {
     var self = this;
-    setTimeout(function(){ check() }, 300);
+    setTimeout(function() { check() }, 300);
 
     var check = function() {
       var invalid = document.querySelectorAll('.is-invalid');
 
       if (invalid[0] == null || invalid[0] == 'undefined') {
-        self.submitBtn[0].removeAttribute("disabled");
+        self.submitBtn[0].removeAttribute('disabled');
       } else {
-        self.submitBtn[0].setAttribute("disabled", true);
+        self.submitBtn[0].setAttribute('disabled', true);
       }
     };
   },
